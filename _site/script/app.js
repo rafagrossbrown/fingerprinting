3a8ff1e7b2d2c54065f4d60bec5fe60d65a76eb9
Rafael Gross Brown Abreu <rgba@MacBookCapitanUK.local>
2016-09-22 14:10:04 +0200

    added jquery for search bar and angular init code to MAIN.JS; added angular directives and scripts to DEFAULT.HTML; INDEX.HTML has a basic angular test that's not working

--- _site/script/app.js
+++ _site/script/app.js
@@ -1,91 +1,87 @@
-var myApp = angular.module('myApp', [
-	'nj-alerts',
-	'nj-modals',
-	'nj-dropdowns'
-]);
 
-myApp.controller('myCtrl', function ($scope, $location, $anchorScroll) {
-  // hash navigation (demo only)
-	$scope.scrollTo = function(id) {
-    var old = $location.hash();
-    $location.hash(id);
-    $anchorScroll();
-    $location.hash(old);
-	};
+	// angular tutorial
 
-	$scope.showData = function() {
-    $scope.datalists = [
-			{"name": "Alerts", "link": "alerts"},
-	    {"name": "Buttons", "link": "buttons"},
-			{"name": "Collapsibles", "link": "collapsibles"},
-			{"name": "Checkboxes", "link": "checkboxes"},
-			{"name": "Radio buttons", "link": "radios"},
-			{"name": "Switches", "link": "switches"},
-			{"name": "Drawers", "link": "drawers"},
-			{"name": "Dropdowns", "link": "dropdowns"},
-			{"name": "Headers", "link": "headers"},
-			{"name": "Icons", "link": "icons"},
-	    {"name": "Inputs", "link": "inputs"},
-			{"name": "Lists", "link": "lists"},
-			// {"name": "Modals", "link": "modals"},
-	    {"name": "Spinners", "link": "spinners"},
-	    {"name": "Typography", "link": "typography"},
-	    {"name": "Grid", "link": "grid"}
-		]
-	};
-  $scope.select= function(item) {
-	  $scope.selected = item;
-	}
-	$scope.isActive = function(item) {
-		return $scope.selected === item;
-	}
+(function() {
+var app= angular.module('store',[] );
+
+app.controller('StoreController', function(){
+
+	this.products= gems;
+
+	});
+
+	var gems = [
+
+		{
+			name: 'Dodecahedron',
+			price: 2.50,
+			description: 'This is crap. ',
+			images: [
+				{full: 'Dodecahedron-01-full.jpg'
+				thumb: 'Dodecahedron-01-thumb.jpg'},
+
+			]
+			canPurchase: true,
+	 
+		},
+		{
+
+			name: 'Pentagonal Gem',
+			price: 5.95,
+			description: 'This is crap. ',
+			canPurchase: true,
+			
+		}
+	];
+
+})();
 
 
-	$scope.collapsed = false;
-  $scope.toggleCollapse = function() {
-    $scope.collapsed = !$scope.collapsed;
-  }
-	$scope.collapsedUsers = false;
-  $scope.toggleCollapseUsers = function() {
-    $scope.collapsedUsers = !$scope.collapsedUsers;
-  }
 
-	$scope.menuOn = false;
-  $scope.toggleMenu = function() {
-    $scope.menuOn = !$scope.menuOn;
-  }
 
-	$scope.demoModal1 = false;
-  $scope.toggleDemoModal1 = function() {
-    $scope.demoModal1 = !$scope.demoModal;
-  };
-	$scope.demoModal2 = false;
-  $scope.toggleDemoModal2 = function() {
-    $scope.demoModal2 = !$scope.demoModal;
-  };
-	$scope.demoModal3 = false;
-  $scope.toggleDemoModal3 = function() {
-    $scope.demoModal3 = !$scope.demoModal;
-  };
-	$scope.demoModal4 = false;
-  $scope.toggleDemoModal4 = function() {
-    $scope.demoModal4 = !$scope.demoModal;
-  };
+	//initialize the 3 popup css class names - create more if needed
+	// var matchClass=['popup1','popup2','popup3'];
+	//Set your 3 basic sizes and other options for the class names above - create more if needed
+	// var popup1 = 'width=400,height=300,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=20,top=20';
+	// var popup2 = 'width=800,height=600,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=20,top=20';
+	// var popup3 = 'width=1000,height=750,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=20,top=20';
+	
+	//The pop-up function
+	// function tfpop(){
+	// 		var x = 0;
+	// 		var popClass;
+			//Cycle through the class names
+			// while(x < matchClass.length){
+			// 		popClass = "'."+matchClass[x]+"'";
+					//Attach the clicks to the popup classes
+					// $(eval(popClass)).click(function() {
+							//Get the destination URL and the class popup specs
+							// var popurl = $(this).attr('href');
+							// var popupSpecs = $(this).attr('class');
+							//Create a "unique" name for the window using a random number
+							// var popupName = Math.floor(Math.random()*10000001);
+							//Opens the pop-up window according to the specified specs
+	// 						newwindow=window.open(popurl,popupName,eval(popupSpecs));
+	// 						return false;
+	// 				});							
+	// 		x++;
+	// 		} 
+	// }
+	
 
-	$scope.demoAlert1 = false;
-  $scope.toggleDemoAlert1 = function() {
-    $scope.demoAlert1 = !$scope.demoAlert1;
-  };
-	$scope.demoAlert2 = false;
-  $scope.toggleDemoAlert2 = function() {
-    $scope.demoAlert2 = !$scope.demoAlert2;
-  };
-	$scope.demoAlert3 = false;
-  $scope.toggleDemoAlert3 = function() {
-    $scope.demoAlert3 = !$scope.demoAlert3;
-  };
-	$scope.demoAlert4 = false;
-  $scope.toggleDemoAlert4 = function() {
-    $scope.demoAlert4 = !$scope.demoAlert4;
-  };
-});
+
+
+	//Wait until the page loads to call the function
+	// $(function() {
+	// 	tfpop();
+			// 		$('#accordion').on('hidden.bs.collapse', function () {
+			 
+			// })
+
+			// $('#accordion .accordion-toggle').click(function (e){
+			//   var chevState = $(e.target).siblings("span.indicator").toggleClass('glyphicon-plus glyphicon-minus');
+			//   $("i.indicator").not(chevState).removeClass("glyphicon-plus").addClass("glyphicon-minus");
+			// });
+	// });
+ 
+
