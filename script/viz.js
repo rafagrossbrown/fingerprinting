7f97ae601ee650af8d3bb0717e8de97037992fb6
Rafael Gross Brown Abreu <rgba@MacBookCapitanUK-2.local>
2016-11-09 12:10:21 +0100

    privacy-meter map js included, other changes

--- script/viz.js
+++ script/viz.js
@@ -1,15 +1,17 @@
 // Visualization 1 -- Global Overview
 //Get website object with a certain url.
 // ALL DATA URL: https://privacymeter-eddbf.firebaseio.com/.json
-var globalData, total_sites,fingerprint_sites; // a global
+var globalData, total_sites,fingerprint_sites, categoryTraffic; // a global
+
+$().append( "<strong>Hello</strong>" );
 
   d3.json("https://privacymeter-eddbf.firebaseio.com/data/global.json", function(json) {
   globalData = json;
-       
 
- 
- 
-      
+
+
+        
+
   // draw global data doughnut viz
   visualizeDataT();
   drawDoughnuts("categories","business","#category1");
@@ -93,146 +95,13 @@ var arcs = group.selectAll(".arc")
             .attr("fill", function(d) { return color(d.data);})
 
  
-// text on arcs
-            // arcs.append("text")
-            //     .attr("transform", function (d) { return "translate(" + arc.centroid(d) + ")";})
-            //     .attr("text-anchor","middle")
-            //     .attr("font-size","24px")
-            //     .attr("fill", "white")
-            //     .text(function(d) { return d.data + " %"  ;});
+ 
 
  
    
 
 
 }
-
-
-// Traffic Map
-
-d3.select(window).on("resize", throttle);
-
-var zoom= d3.zoom()
-            .scaleExtent([1,8])
-            .on("zoom",move);
-
-var width= document.getElementById('containerMap').offsetWidth-20;
-var height= width /2;
-
-var topo,projection,path,svg,g;
-
-
-
-setup(width,height);
-
-function setup(width,height){
-  projection= d3.geoMercator()
-                .translate([0,0])
-                .scale(width /2 / Math.PI);
-
-  path= d3.geoPath()
-          .projection(projection);
-
-  svg=d3.select("#containerMap").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-        .attr("transform", "translate(" + width/2 + "," + height /2 + ")")
-        // .call(zoom);
-
-g = svg.append("g");
-
-}
- 
-d3.json("data/world-topo.json", function (error, world) {
-
- 
-  // var countries = world.objects;
-  //topojson.feature  returns a features collection
-  var countries= topojson.feature(world, world.objects.countries).features;
-
-  topo = countries;
-
-  draw(topo);
-
- 
- 
-   
-
-});                
-
-// draw function
-
-function draw(topo){
-
-var country= g.selectAll(".country").data(topo);
-
-country.enter().insert("path")
-      .attr("class","country")
-      .attr("d",path)
-      .attr("id", function(d,i) { return d.id; })
-      .attr("title", function (d,i) {return d.properties.name})
-     .style("fill", "white");
-   
-     
-      // .style("fill", function(d,i) {return d.properties.color;});
-
-  //ofsets plus width/height of transform, plsu 20 px of padding, plus 20 extra for tooltip offset off mouse
-  var offsetL = document.getElementById('containerMap').offsetLeft+(width/2)+40;
-  var offsetT =document.getElementById('containerMap').offsetTop+(height/2)+20;
-
-//tooltips
-country.on("mousemove", function(d,i){
-          var mouse= d3.mouse(svg.node()).map(function (d) {return parseInt (d);});
-            tooltip.classed("hidden",false)
-              .attr("style", "left:" + (mouse[0] + offsetL) + "px;top:"+ (mouse[1]+offsetT)+ "px")
-              .html(d.properties.name)
-          })
-        .on("mouseout", function(d,i){
-          tooltip.classed("hidden",true)
-        });
-}
-
-function redraw() {
-  width= document.getElementById('containerMap').offsetWidth-60;
-  height= width/2;
-  d3.select('svg').remove();
-  setup(width,height);
-  draw(topo);
-}
-
-
-//move() es el callback del var zoom
-function move(){
-
-  var t= d3.event.translate;
-  var s = d3.event.scale;
-  var h = height/3;
-
-  t[0]= Math.min(width/ 2 * (s-1), Math.max(width /2 * (1-s), t[0]));
-  t[1]= Math.min(height /2 * (s-1) + h * s, Math.max(height/2 *(1-s) - h * s, t[1]));
-
-  zoom.translate(t);
-  g.style("stroke-width",1/s).attr("transform", "translate(" + t + ")scale(" + s + ")");
-
-
-}
-
-var throttleTimer;
-
-function throttle(){
-  window.clearTimeout(throttleTimer);
-    throttleTimer = window.setTimeout(function(){
-      redraw();
-    }, 200);
-
-    }
-//closemap 
-
-
-
-
-
 
 
     //Fingerprinted Traffic by Website Category
@@ -257,13 +126,34 @@ function drawBargraph() {
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
 // URL FOR CATEGORIES: https://privacymeter-eddbf.firebaseio.com/data/global/categories.json      
-    d3.json("horizontalData.json", function(error, data1) {
+   
+
+//     
+
+// console.log(data2);
+//     });
+// d3.json("horizontalData.json", function(error, data1) {
+
+d3.json("https://privacymeter-eddbf.firebaseio.com/data/global/top_ranks/top_categories_traffic.json", function(error,data1){
+
+          // var keyArray=d3.keys(data1);
+
+          // console.log(keyArray[0]);
+// console.log(data1);
+//For loop que busque data1[i]
+          
+
+// console.log("bargraph a trafficValue =" + "  "+ trafficValue);
+// data1[query][target].tracking_traffic; 
+
+
         if (error) throw error;
+      // sort data from max to min:
+          data1.sort(function(a, b) { return a.value - b.value; });
       
-        data1.sort(function(a, b) { return a.value - b.value; });
       //add the domain data
         x.domain([0, d3.max(data1, function(d) { return d.value; })]);
-        y.domain(data1.map(function(d) { return d.category; }))
+        y.domain(data1.map(function(d) { return d.collection; }))
           .padding(0.08);
 
 //Append AXES to svg group
@@ -285,7 +175,7 @@ function drawBargraph() {
             .attr("class", "bar")
             .attr("x", 0)
             .attr("height", y.bandwidth())
-            .attr("y", function(d) { return y(d.category); })
+            .attr("y", function(d) { return y(d.collection); })
             .attr("width", function(d) { return x(d.value); })
             .on("mousemove", function(d){
           // Replace hard coded vals (50, 90) with 50% of the tooltip wioth and height + a top buffer
@@ -293,7 +183,7 @@ function drawBargraph() {
                   .style("left", d3.event.pageX - 50 + "px")
                   .style("top", d3.event.pageY - 90 + "px")
                   .style("display", "inline-block")
-                  .html((d.category) + "<br><span>" + (d.value) + "   "+ "tracked visits" + "</span>");
+                  .html((d.collection) + "<br><span>" + (d.value) + "   "+ "tracked visits" + "</span>");
             })
             .on("mouseout", function(d){ tooltip.style("display", "none");})
 
@@ -311,8 +201,6 @@ function drawDoughnuts(query,target,catDiv){
 // thevariable data is defined as an argument in the callback function of d3 json
 // d3.json("mydata.json", function(data) {
  
-
-
 
 
 // 1- get # of total news analyzed
